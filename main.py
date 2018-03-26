196741391cb4bfcdd8336ca19349b4c3f3889faf
Al Crowley <albert.crowley@tcg.com>
2018-03-26 14:00:30 -0400

    Adding a background

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.uix.label import Label
 from kivy.core.window import Window
+from kivy.uix.image import Image
 
 # from game_utils import params
 # from game_utils import Sprite
@@ -19,11 +20,35 @@ from kivy.core.window import Window
 import math
 from random import randint
 
-# kivy.require('1.10.0')
+class Sprite(Image):
+    def __init__(self, **kwargs):
+        super(Sprite, self).__init__(allow_stretch=True, **kwargs)
+        self.size = (self.texture.width, self.texture.height)
+        self.texture.mag_filter = 'nearest'
+
+
+class Game(Widget):
+    def __init__(self):
+        super(Game, self).__init__()
+        self.background = Sprite(source="background.png", pos=(0,0))
+        self.background.pos = (0,0)
+        self.add_widget(self.background)
+
+        Clock.schedule_interval(self.update, 1.0 / 60.0)
+        print ("done")
+
+
+    def update(self, dt):
+        pass
+
+
+
+
+
 
 class Runner(App):
     def build(self):
-        game = Widget()
+        game = Game()
         return game
 
 
