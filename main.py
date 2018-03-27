98064987ffec04476426158e864d2a2a55f084fc
Al Crowley <albert.crowley@tcg.com>
2018-03-26 23:08:39 -0400

    adding runner

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ from kivy.clock import Clock
 from kivy.uix.label import Label
 from kivy.core.window import Window
 from kivy.uix.image import Image
+from kivy.atlas import Atlas, Logger
+import os
+import json
+from os.path import dirname, join
+from kivy.core.image import Image as CoreImage
 
-# from game_utils import params
-# from game_utils import Sprite
-# from game_utils import SpriteAtlas
-# from game_utils import Background
-# from game_utils import Blank
 
 import math
 from random import randint
@@ -26,6 +26,57 @@ class Sprite(Image):
         self.size = (self.texture.width, self.texture.height)
         self.texture.mag_filter = 'nearest'
 
+
+class SpriteAtlas(Atlas):
+    def _load(self):
+        # must be a name finished by .atlas ?
+        filename = self._filename
+        assert(filename.endswith('.atlas'))
+        filename = filename.replace('/', os.sep)
+
+        Logger.debug('Atlas: Load <%s>' % filename)
+        with open(filename, 'r') as fd:
+            meta = json.load(fd)
+
+        Logger.debug('Atlas: Need to load %d images' % len(meta))
+        d = dirname(filename)
+        textures = {}
+        for subfilename, ids in meta.items():
+            subfilename = join(d, subfilename)
+            Logger.debug('Atlas: Load <%s>' % subfilename)
+
+            # load the image
+            ci = CoreImage(subfilename)
+
+            # <RJ> this is the fix for pixel art
+            ci.texture.mag_filter = 'nearest'
+
+            # for all the uid, load the image, get the region, and put
+            # it in our dict.
+            for meta_id, meta_coords in ids.items():
+                x, y, w, h = meta_coords
+                textures[meta_id] = ci.texture.get_region(*meta_coords)
+
+        self.textures = textures
+
+
+class Player(Sprite):
+
+    def __init__(self, pos):
+        self.images = SpriteAtlas('RUN.atlas')
+        super(Player, self).__init__(texture=self.images['1'], pos=pos)
+        self.tick = 1.0/60.0
+        self.ticks = 0;
+        self.height *= 2
+        self.width *= 2
+
+    def update(self, dt):
+        self.ticks += 1
+        frame = int(math.floor( self.ticks / 10)) % 8
+        self.texture = self.images[str(frame)]
+
+
+
 class Background(Widget):
     def __init__(self, source):
         super(Background, self).__init__()
@@ -34,7 +85,7 @@ class Background(Widget):
         self.size = self.image.size
         self.image_dupe = Sprite(source=source, x=self.width)
         self.add_widget(self.image_dupe)
-        self.speed = 6;
+        self.speed = 3;
         self.tick = 1.0/60.0
 
     def update(self, dt):
@@ -48,15 +99,16 @@ class Background(Widget):
 class Game(Widget):
     def __init__(self):
         super(Game, self).__init__()
+        Clock.schedule_interval(self.update, 1.0 / 60.0)
         self.background = Background(source="background.png")
         self.add_widget(self.background)
-
-        Clock.schedule_interval(self.update, 1.0 / 60.0)
-        print ("done")
+        self.player = Player(pos=(100,-0.1))
+        self.add_widget(self.player)
 
 
     def update(self, dt):
         self.background.update(dt=dt)
+        self.player.update(dt=dt)
 
 
 
